bdc2dd0e656b318d4822b2b0bf102792bdc01883
participator <vjhawkins@yahoo.com>
2020-01-28 01:16:35 -0800

    Update video page
    - Add active background on page load
    - Update active background color to match nav and subscribe background color

--- src/js/video_player.js
+++ src/js/video_player.js
@@ -1,11 +1,25 @@
 (function() {
-    const videoItems = document.getElementsByClassName('bizday_videos_items');
-    videoItems[0].children.forEach(element => {
+    const activeClass = 'bizday_videos_item--active';
+    const videoItems = document.getElementsByClassName('bizday_videos_items')[0];
+
+    videoItems.children.forEach(element => {
         element.addEventListener('click', (event) => {
-            videoItems[0].children.forEach(element => {
-                element.classList.remove('bizday_videos_item--active');
+            videoItems.children.forEach(element => {
+                element.classList.remove(activeClass);
             })
-            event.currentTarget.classList.add('bizday_videos_item--active');
+            event.currentTarget.classList.add(activeClass);
         })        
     });
+
+    window.onload = function() {
+        const videoPlayerViewer = document.getElementsByClassName('bizday_videos_player_viewer')[0];
+
+        const initialVideoUrl = videoPlayerViewer.src;
+
+        videoItems.children.forEach(element => {
+            if ( element.href === initialVideoUrl ) {
+                element.classList.add(activeClass);
+            }
+        })
+    }
 })()
